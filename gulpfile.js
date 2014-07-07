3d5561b7ec6b3e2cc4db18966e666aa7c5cec70f
Ricardo Mota <ricardo.mota@nix-digital.com>
2014-07-07 12:24:39 +0800

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,70 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
+var coffee = require('gulp-coffee');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var connect = require('gulp-connect');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['source/scss/*.scss'],
+  coffee: ['source/coffee/*.coffee'],
+  css: ['www/**/*.css'],
+  html: ['www/index.html', 'www/templates/**/*.html']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['watch', 'server']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
+  // gulp.src('./source/scss/ionic.app.scss')
+  gulp.src(paths.sass)
+    .pipe(sass().on('error', function(e){ gutil.log(e); this.end(); }))
     .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    // .pipe(minifyCss({
+    //   keepSpecialComments: 0
+    // }))
+    // .pipe(rename({ extname: '.min.css' }))
+    // .pipe(gulp.dest('./www/css/'))
+    .pipe(connect.reload())
+    .on('end', done);
+});
+
+gulp.task('coffee', function(done) {
+  gulp.src(paths.coffee)
+    .pipe(coffee().on('error', function(e){ gutil.log(e); this.end(); }))
+    .pipe(gulp.dest('./www/js/'))
+    .pipe(connect.reload())
+    .on('end', done);
+});
+
+gulp.task('css', function(done) {
+  gulp.src(paths.css)
+    .pipe(connect.reload())
+    .on('end', done);
+});
+
+gulp.task('html', function(done) {
+  gulp.src(paths.html)
+    .pipe(connect.reload())
     .on('end', done);
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+  // gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.coffee, ['coffee']);
+  gulp.watch(paths.css, ['css']);
+  gulp.watch(paths.html, ['html']);
+});
+
+gulp.task('server', function(){
+  connect.server({
+    root: ['www'],
+    hostname: '0.0.0.0',
+    port: 9100,
+    livereload: true
+  });
 });
 
 gulp.task('install', ['git-check'], function() {
